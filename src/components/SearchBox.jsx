773044f2b486d2539ea3c2c17569a0ada835ba95
Abdelhaq El Amraoui <salmahj936@gmail.com>
2025-07-03 11:44:27 +0100

    style search box

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -13,18 +13,18 @@ export default function SearchBox() {
 
    return (
       <form
-         className="flex justify-between px-5 max-w-6xl mx-auto"
+         className="flex justify-between px-5 max-w-6xl mx-auto mt-4"
          onSubmit={(e) => handleSubmit(e)}
       >
          <input
             type="text"
             placeholder="Search keywords..."
-            className="w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1"
+            className="w-full h-10 placeholder-gray-500 outline-none bg-transparent flex-1 border-b border-b-amber-600"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
          />
          <button
-            className="text-amber-600 disabled:text-gray-400"
+            className=" disabled:text-gray-600 disabled:opacity-90 disabled:cursor-auto hover:opacity-90 cursor-pointer bg-amber-500 px-4 rounded-lg ml-2 dark:text-white"
             disabled={search === ""}
          >
             Search
